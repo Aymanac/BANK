174e3eb2d24c0d0c898e2fa02cbbc62559ee0b72
Aymanac <ayman.achbal@gmail.com>
2024-01-11 20:52:38 +0100

    database linking.2

--- inscription.cpp
+++ inscription.cpp
@@ -12,3 +12,24 @@ Inscription::~Inscription()
 {
     delete ui;
 }
+
+
+
+void Inscription::on_Sinscrire_clicked()
+{
+    QSqlDatabase database ;
+    database = QSqlDatabase::addDatabase("QMYSQL");
+    database.setHostName("127.0.0.1");
+    database.setUserName("root");
+    database.setPassword("");
+    database.setDatabaseName("clients");
+
+    if(database.open())  {
+
+        QMessageBox::information(this,"Connected", "Database Connected") ;
+    }else {
+
+        QMessageBox::information(this,"Not Connected", "Database Is Not Connected") ;
+    }
+}
+
--- inscription.h
+++ inscription.h
@@ -2,6 +2,9 @@
 #define INSCRIPTION_H
 
 #include <QDialog>
+#include <QtSql/QSql>
+#include <QtSql/QSqlDatabase>
+#include <QMessageBox>
 
 namespace Ui {
 class Inscription;
@@ -15,8 +18,13 @@ public:
     explicit Inscription(QWidget *parent = nullptr);
     ~Inscription();
 
+private slots:
+
+    void on_Sinscrire_clicked();
+
 private:
     Ui::Inscription *ui;
+    QSqlDatabase database ;
 };
 
 #endif // INSCRIPTION_H
